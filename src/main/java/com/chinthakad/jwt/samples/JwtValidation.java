78b72f9e1326ffdd1fda8fa4a3a05cbefb4baba5
Chinthaka Dharmasiri <chinthakadd@gmail.com>
2020-11-16 10:46:37 -0500

    JWT Validation - How To

--- src/main/java/com/chinthakad/jwt/samples/JwtValidation.java
+++ src/main/java/com/chinthakad/jwt/samples/JwtValidation.java
@@ -2,20 +2,26 @@ package com.chinthakad.jwt.samples;
 
 import com.nimbusds.jose.*;
 import com.nimbusds.jose.crypto.RSASSASigner;
+import com.nimbusds.jose.crypto.RSASSAVerifier;
+import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jwt.JWTClaimsSet;
 
 import java.io.IOException;
 import java.security.*;
 import java.security.cert.CertificateException;
 import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.text.ParseException;
 import java.time.Instant;
 import java.util.Date;
 import java.util.Objects;
 
-public class JwtSample {
+public class JwtValidation {
 
     public static void main(String[] args) throws KeyStoreException, CertificateException,
-            NoSuchAlgorithmException, IOException, UnrecoverableKeyException, JOSEException {
+            NoSuchAlgorithmException, IOException, UnrecoverableKeyException, JOSEException, ParseException {
+        // =====================
+        // SIGNATURE
         JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.RS256)
                 .type(JOSEObjectType.JWT)
                 .build();
@@ -24,7 +30,7 @@ public class JwtSample {
                 .issuer("me")
                 .audience("you")
                 .subject("bob")
-                .expirationTime(Date.from(Instant.now().plusSeconds(5)))
+                .expirationTime(Date.from(Instant.now().minusSeconds(5)))
                 .build();
 
         JWSObject jwsObj = new JWSObject(header, new Payload(payload.toJSONObject()));
@@ -39,6 +45,20 @@ public class JwtSample {
         RSASSASigner rsassaSigner = new RSASSASigner(key);
         jwsObj.sign(rsassaSigner);
 
-        System.out.println(jwsObj.serialize());
+
+        // =====================
+        // VALIDATION
+        String jwt = jwsObj.serialize();
+
+        ks.load(JwtSample.class.getClassLoader().getResourceAsStream("default.jks"), jksPassword.toCharArray());
+        RSAPublicKey publicKey = (RSAPublicKey) ks.getCertificate("com.chinthakad").getPublicKey();
+
+        assert publicKey != null;
+        RSAKey rsaKey = new RSAKey.Builder(publicKey).build();
+        JWSVerifier verifier = new RSASSAVerifier(rsaKey);
+        JWSObject jwtObj = JWSObject.parse(jwt);
+        System.out.println(jwtObj.verify(verifier));
+        System.out.println(jwtObj.getPayload().toJSONObject().toJSONString());
+
     }
 }
